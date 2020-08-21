a615278f7eda5ee46448d5fdaef2f7b9818864ae
Colin McNamara <coolinmc6@gmail.com>
2020-08-20 20:39:41 -0400

    final tweaks

--- src/MyChart.js
+++ src/MyChart.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-
+import { qdtCompose, QdtPicasso, useBarChartSettings, usePieChartSettings, QdtButton } from 'qdt-components';
 import appPromise from './config/app';
 
 appPromise.then((app) => {
@@ -26,10 +26,54 @@ const hypercube = {
 }
 
 const MyChart = () => {
+  const barChart = useRef(null);
+  const pieChart = useRef(null);
+  const clearButton = useRef(null);
 
+  useEffect(() => {
+    (async () => {
+      const settings = useBarChartSettings();
+      const app = await appPromise;
+      qdtCompose({
+        app,
+        element: barChart.current,
+        component: QdtPicasso,
+        options: {
+          settings,
+        },
+        properties: hypercube,
+        loading: () => null,
+      })
+
+      const pieSettings = usePieChartSettings();
+      qdtCompose({
+        app,
+        element: pieChart.current,
+        component: QdtPicasso,
+        options: {
+          settings: pieSettings,
+        },
+        properties: hypercube,
+        loading: () => null,
+      })
+
+      qdtCompose({
+        app, 
+        element: clearButton.current, 
+        component: QdtButton,
+        options: {
+          type: 'clearSelections',
+          label: 'Clear Selections'
+        }
+      })
+    })()
+  }, [])
   return (
     <div>
       <h2>My Charts</h2>
+      <div ref={clearButton} style={{ margin: '10px auto', width: 250 }} />
+      <div ref={barChart} style={{ height: 400, paddingTop: 50 }} />
+      <div ref={pieChart} style={{ height: 400, paddingTop: 50 }} />
     </div>
   )
 }
